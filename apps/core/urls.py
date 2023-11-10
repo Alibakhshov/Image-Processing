38d39373e88fddcca62893d8853a721e8664bbca
Rauf Alibakhshov <91628590+Alibakhshov@users.noreply.github.com>
2023-11-10 16:07:04 +0600

    Adding cropping part

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import dashboard
+from .views import dashboard, cropping
 
 urlpatterns = [
     path("", dashboard, name="dashboard"),
+    path("cropping/", cropping, name="cropping"),
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def dashboard(request):
     return render(request, "dashboard.html")
+
+def cropping(request):
+    return render(request, "pages/advanced-ui/cropper.html")
