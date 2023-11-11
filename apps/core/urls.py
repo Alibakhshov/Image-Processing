41b716aa50356e39d5f773b42732409f91a13d04
Rauf Alibakhshov <91628590+Alibakhshov@users.noreply.github.com>
2023-11-11 12:46:45 +0600

    Updates

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
     path("cropping/", views.cropping, name="cropping"),
-    path('upload/', views.upload_image, name='upload_image'),
+    path('NearestNeighborInterpolation/', views.upload_image, name='NearestNeighborInterpolation'),
     path('image/<int:pk>/', views.image_detail, name='image_detail'),
     path('delete/<int:pk>/', views.delete_image, name='delete_image'),
     path('save/<int:pk>/', views.save_image, name='save_image'),
